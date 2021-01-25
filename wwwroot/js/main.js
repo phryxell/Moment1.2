ad3946c0341d9258f384fcc17d7ff37d4283a441
philipfryxell <philipnordstrom@outlook.com>
2021-01-25 15:21:28 +0100

    Add project files.

---
+++ wwwroot/js/main.js
@@ -0,0 +1,9 @@
+﻿"use strict"
+
+function toggleMenu() {
+    let navigation = document.querySelector('.navigation');
+    let toggle = document.querySelector('.toggle');
+    navigation.classList.toggle('active');
+   toggle.classList.toggle('active');
+
+}
